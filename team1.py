d5d68b47c6dafe833b3d6a216d8d9b97c64ceab4
manvendra-nema <manvendra.nema1@gamil.com>
2024-09-10 12:22:08 +0530

    template compleate

--- team1.py
+++ team1.py
@@ -12,5 +12,12 @@ def add(list_num):
 
 
 def main():
-    pass
+    nums_liz = generate_num()
+    print(nums_liz)
+    sum = add(nums_liz)
+    print(sum)
+    mean = avg(nums_liz)
+    print(mean)
+    std = sd(nums_liz)
+    print(std)
 main()
